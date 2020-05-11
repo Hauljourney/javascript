6bba2a27f20316240c72cc2b55ca4e3c4cb98fe4
arda ozgul <arda.ozgul@mrevyap.k12.tr>
2020-05-11 16:25:49 +0300

    first commit

---
+++ Ödev1/javascript.js
@@ -0,0 +1,7 @@
+var cek = document.getElementById("metin");
+
+function tikla()
+{
+cek.style.color="red";
+cek.style.fontFamily="Century Gothic";
+}
---
+++ Ödev2/javascript.js
@@ -0,0 +1,9 @@
+
+
+function degerlerial()
+{  
+    var cek = document.getElementById("form1")[0].value;
+    alert(cek);
+    cek = document.getElementById("form1")[1].value;
+    alert(cek);
+}
